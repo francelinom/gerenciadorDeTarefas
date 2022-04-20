bf0287d211488345315b375aa8168dd2844ec65c
francelino <fmarcilio1989@gmail.com>
2022-04-20 08:49:22 -0300

    component de listar tarefas

--- src/app/tarefas/tarefas.module.ts
+++ src/app/tarefas/tarefas.module.ts
@@ -1,9 +1,12 @@
 import { TarefasService } from './shared/tarefas.service';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ListarTarefaComponent } from './listar-tarefa/listar-tarefa.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ListarTarefaComponent
+  ],
   imports: [CommonModule],
   providers: [TarefasService],
 })
