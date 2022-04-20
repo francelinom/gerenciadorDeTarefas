45bbad64884e64c391095b7e992023cc7fcced83
francelino <fmarcilio1989@gmail.com>
2022-04-20 13:28:53 -0300

    component de criar nova tarefa e fazendo o link com o botão de novo

--- src/app/tarefas/tarefas-routing.module.ts
+++ src/app/tarefas/tarefas-routing.module.ts
@@ -1,3 +1,4 @@
+import { CadastrarTarefaComponent } from './cadastrar-tarefa/cadastrar-tarefa.component';
 import { Routes } from '@angular/router';
 
 import { ListarTarefaComponent } from './listar-tarefa/listar-tarefa.component';
@@ -10,5 +11,9 @@ export const TarefaRoutes: Routes = [
   {
     path: 'tarefas/listar',
     component: ListarTarefaComponent,
+  },
+  {
+    path: 'tarefas/cadastrar',
+    component: CadastrarTarefaComponent,
   },
 ];
--- src/app/tarefas/tarefas.module.ts
+++ src/app/tarefas/tarefas.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListarTarefaComponent } from './listar-tarefa/listar-tarefa.component';
 import { FormsModule } from '@angular/forms';
+import { CadastrarTarefaComponent } from './cadastrar-tarefa/cadastrar-tarefa.component';
 
 @NgModule({
-  declarations: [ListarTarefaComponent],
+  declarations: [ListarTarefaComponent, CadastrarTarefaComponent],
   imports: [CommonModule, RouterModule, FormsModule],
   providers: [TarefasService],
 })
