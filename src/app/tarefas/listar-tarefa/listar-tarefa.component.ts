bb684bd69553fd0e06e67dccf45f8259a1f29c9e
francelino <fmarcilio1989@gmail.com>
2022-04-20 09:55:28 -0300

    metodos de listar e remover tarefas

--- src/app/tarefas/listar-tarefa/listar-tarefa.component.ts
+++ src/app/tarefas/listar-tarefa/listar-tarefa.component.ts
@@ -1,15 +1,30 @@
+import { Tarefa } from './../shared/tarefa.model';
+import { TarefasService } from './../shared/tarefas.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-listar-tarefa',
   templateUrl: './listar-tarefa.component.html',
-  styleUrls: ['./listar-tarefa.component.css']
+  styleUrls: ['./listar-tarefa.component.css'],
 })
 export class ListarTarefaComponent implements OnInit {
-
-  constructor() { }
+  tarefas!: Tarefa[];
+  constructor(private tarefaService: TarefasService) {}
 
   ngOnInit(): void {
+    this.tarefas = this.listarTodos();
   }
 
+  listarTodos(): Tarefa[] {
+    return this.tarefaService.listarTodos();
+  }
+
+  remover($event: any, tarefa: any): void {
+    $event.preventDefault();
+
+    if (confirm('Deseja remover a tarefa "' + tarefa.nome + '"? ')) {
+      this.tarefaService.remover(tarefa.id);
+      this.tarefas = this.tarefaService.listarTodos();
+    }
+  }
 }
--- src/app/tarefas/tarefas.module.ts
+++ src/app/tarefas/tarefas.module.ts
@@ -1,11 +1,13 @@
+import { RouterModule } from '@angular/router';
 import { TarefasService } from './shared/tarefas.service';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListarTarefaComponent } from './listar-tarefa/listar-tarefa.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [ListarTarefaComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule, FormsModule],
   providers: [TarefasService],
 })
 export class TarefasModule {}
