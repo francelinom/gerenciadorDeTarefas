d3808a455308dcd8a4cb9ed6e7e362eff9f0efcd
francelino <fmarcilio1989@gmail.com>
2022-04-20 17:13:10 -0300

    implementando o remover e o alterar status

--- src/app/tarefas/listar-tarefa/listar-tarefa.component.ts
+++ src/app/tarefas/listar-tarefa/listar-tarefa.component.ts
@@ -24,6 +24,13 @@ export class ListarTarefaComponent implements OnInit {
 
     if (confirm('Deseja remover a tarefa "' + tarefa.nome + '"? ')) {
       this.tarefaService.remover(tarefa.id);
+      this.tarefas = this.listarTodos();
+    }
+  }
+
+  alterarStatus(tarefa: any): void {
+    if (confirm('Deseja alterar o status da tarefa "' + tarefa.nome + '" ?')) {
+      this.tarefaService.alterarStatus(tarefa.id);
       this.tarefas = this.tarefaService.listarTodos();
     }
   }
